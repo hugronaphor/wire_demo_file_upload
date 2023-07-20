3dc2aa754a424af21945a18ab5663b3a1fdeb252
cornel <hugronaphor@gmail.com>
2023-07-20 13:45:22 +0300

    Refactor with rescue() func.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Wire Demo File Upload
 
-This Drupal module provides a demo of how to upload files via Drag&Drop using the Wire.
+This Drupal module provides a demo of how to upload files via Drag & Drop using Wire.
--- src/Plugin/WireComponent/DragAndDropFileUpload.php
+++ src/Plugin/WireComponent/DragAndDropFileUpload.php
@@ -3,19 +3,21 @@
 namespace Drupal\wire_demo_file_upload\Plugin\WireComponent;
 
 use Drupal\file\Entity\File;
+use Drupal\file\FileInterface;
 use Drupal\wire\TemporaryUploadedFile;
 use Drupal\wire\View;
 use Drupal\wire\WireComponent;
 use Drupal\wire\WithFileUploads;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
+use function Hugronaphor\PhpHelpers\rescue;
 
 /**
  * Implementation for DragAndDropFileUpload Wire Component.
  *
  * @WireComponent(
  *   id = "drag_and_drop_file_upload",
- *   label = @Translation("DragAndDropFileUpload"),
+ *   label = @Translation("Drag And Drop File Upload"),
  * )
  */
 class DragAndDropFileUpload extends WireComponent {
@@ -26,9 +28,9 @@ class DragAndDropFileUpload extends WireComponent {
 
   public $myFile = NULL;
 
-  public string $message;
+  public ?string $successMessage;
 
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
     $instance = new static($configuration, $plugin_id, $plugin_definition);
     $instance->account = $container->get('current_user');
     return $instance;
@@ -37,43 +39,40 @@ class DragAndDropFileUpload extends WireComponent {
   public function save(): void {
 
     $this->resetValidation();
-    $this->message = '';
 
-    try {
-      if ($file = $this->getFile()) {
-
-        $file->save();
+    $this->successMessage = rescue(
+      callback: function () {
+        $tempFile = $this->myFile;
         $this->myFile = NULL;
 
-        if (!$file->id()) {
-          $this->myFile = NULL;
-          throw new \Exception('File could not be saved.');
+        if (!$file = $this->getFile($tempFile)) {
+          $this->addError('myFile', 'Please select a file.');
+          return NULL;
         }
 
-        $this->message = sprintf('File created successfully with ID: %s.', $file->id());
-      }
-      else {
-        $this->addError('myFile', 'Please select a file.');
-      }
+        $file->save();
+        throw_if(!$file instanceof FileInterface, new \Exception('File could not be saved.'));
 
-    } catch (\Exception $e) {
-      watchdog_exception('wire_demo_file_upload', $e);
-      $this->addError('myFile', 'Something went wrong while saving the file.');
-    }
+        return sprintf('File created successfully with ID: %s.', $file->id());
+      },
+      rescue: function () {
+        $this->myFile = NULL;
+        $this->addError('myFile', 'Something went wrong while saving the file.');
+      },
+    );
 
   }
 
   public function updatingMyFile(): void {
-    $this->message = '';
+    $this->successMessage = NULL;
   }
 
   public function removeMyFile(): void {
     isset($this->myFile) && $this->removeUpload('myFile', $this->myFile->getFilename());
   }
 
-  private function getFile(): ?File {
+  private function getFile($tempFile): ?File {
 
-    $tempFile = $this->myFile ?? NULL;
     if (!$tempFile instanceof TemporaryUploadedFile) {
       return NULL;
     }
